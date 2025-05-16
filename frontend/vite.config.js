8b080bf584497c22dc0de5af0da208c6090ca60b
BLOCK-PROGRAMR <0xscater@gmail.com>
2025-05-16 20:08:54 +0530

    viteconfig changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,24 +1,48 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     // Reduce chunk size
+//     chunkSizeWarningLimit: 1000,
+//     // Use esbuild for minification (faster and uses less memory than terser)
+//     minify: 'esbuild',
+//     // Split chunks more aggressively
+//     rollupOptions: {
+//       output: {
+//         manualChunks: (id) => {
+//           // Split node_modules into smaller chunks
+//           if (id.includes('node_modules')) {
+//             if (id.includes('react')) return 'vendor-react'
+//             if (id.includes('redux')) return 'vendor-redux'
+//             if (id.includes('icons')) return 'vendor-icons'
+//             return 'vendor' // all other packages
+//           }
+//         },
+//       },
+//     },
+//   },
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // 🔥 REQUIRED for static deployment like Render
   plugins: [react()],
   build: {
-    // Reduce chunk size
     chunkSizeWarningLimit: 1000,
-    // Use esbuild for minification (faster and uses less memory than terser)
     minify: 'esbuild',
-    // Split chunks more aggressively
     rollupOptions: {
       output: {
         manualChunks: (id) => {
-          // Split node_modules into smaller chunks
           if (id.includes('node_modules')) {
             if (id.includes('react')) return 'vendor-react'
             if (id.includes('redux')) return 'vendor-redux'
             if (id.includes('icons')) return 'vendor-icons'
-            return 'vendor' // all other packages
+            return 'vendor'
           }
         },
       },
